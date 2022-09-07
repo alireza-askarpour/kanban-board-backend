5e262ea3535727e67d87b0bcd1350279423766a4
Alireza Askarpour <askarpourdev@gmail.com>
2022-09-07 13:34:31 -0700

    feat: add update task controller

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -16,3 +16,14 @@ export const createTask = async (req, res, next) => {
     next(err)
   }
 }
+
+export const updateTask = async (req, res, next) => {
+  const { taskId } = req.params
+  try {
+    const task = await TaskModel.findByIdAndUpdate(taskId, { $set: req.body })
+    res.status(200).json(task)
+  } catch (err) {
+    next(err)
+  }
+}
+
