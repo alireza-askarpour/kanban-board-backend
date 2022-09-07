cb9e157497b356f1c5667f91a42db4c23f41367c
Alireza Askarpour <askarpourdev@gmail.com>
2022-09-07 12:53:28 -0700

    faet: add task validation

---
+++ src/validations/task.js
@@ -0,0 +1,45 @@
+import { param, body } from 'express-validator'
+import { isObjectId } from '../utils/validation.js'
+
+export const createValidation = () => {
+  return [
+    param('boardId').custom((value) => {
+      if (!isObjectId(value)) throw 'invalid board id'
+      return true
+    }),
+    body('sectionId').custom((value) => {
+      if (!isObjectId(value)) throw 'invalid section id'
+    }),
+  ]
+}
+
+export const upadtePositionValidation = () => {
+  return [
+    param('boardId').custom((value) => {
+      if (!isObjectId(value)) throw 'invalid board id'
+      return true
+    }),
+  ]
+}
+
+export const deleteValidation = () => {
+  return [
+    param('boardId').custom((value) => {
+      if (!isObjectId(value)) throw 'invalid board id'
+      return true
+    }),
+  ]
+}
+
+export const updateValidation = () => {
+  return [
+    param('boardId').custom((value) => {
+      if (!isObjectId(value)) throw 'invalid board id'
+      return true
+    }),
+    param('sectionId').custom((value) => {
+      if (!isObjectId(value)) throw 'invalid section id'
+      return true
+    }),
+  ]
+}
