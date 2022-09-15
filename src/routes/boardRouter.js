cd9cb31a7a2af6bb48a00d84de1d313eda6a7bc3
Alireza Askarpour <askarpourdev@gmail.com>
2022-09-15 10:05:49 -0700

    fix: call boardIdParamValidation function in board router

--- src/routes/boardRouter.js
+++ src/routes/boardRouter.js
@@ -11,8 +11,8 @@ router.get('/', verifyToken, boardController.getAll)
 router.put('/update-position', verifyToken, boardController.updatePosition)
 router.get('/favourites', verifyToken, boardController.getFavourites)
 router.put('/favourites', verifyToken, boardController.updateFavouritePosition)
-router.get('/:boardId', boardIdParamValidation, validate, verifyToken, boardController.getOne)
-router.put('/:boardId', boardIdParamValidation, validate, verifyToken, boardController.update)
-router.delete('/:boardId', boardIdParamValidation, validate, verifyToken, boardController.deleteBoard)
+router.get('/:boardId', boardIdParamValidation(), validate, verifyToken, boardController.getOne)
+router.put('/:boardId', boardIdParamValidation(), validate, verifyToken, boardController.update)
+router.delete('/:boardId', boardIdParamValidation(), validate, verifyToken, boardController.deleteBoard)
 
 export default router
