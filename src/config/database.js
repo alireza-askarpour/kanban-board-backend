a227047de1023a4b1a44b47b9216dab7c5e5a7dd
Alireza Askarpour <askarpourdev@gmail.com>
2023-01-30 21:13:52 +0330

    refactor: add config database

--- src/config/database.js
+++ src/config/database.js
@@ -4,6 +4,19 @@ const connectDB = () => {
   mongoose.connect(process.env.MONGO_URI, () => {
     console.log(`MongoDB Connected!`)
   })
+
+  mongoose.connection.on('connected', () => {
+    console.log('mongoose connected to DB')
+  })
+
+  mongoose.connection.on('disconnected', () => {
+    console.log('mongoose disconnected')
+  })
+
+  process.on('SIGINT', async () => {
+    await mongoose.connection.close()
+    process.exit(0)
+  })
 }
 
 export default connectDB
