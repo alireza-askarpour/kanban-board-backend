39ede4d1c9ecc7b7638f539bad55671b9d1b7118
Alireza Askarpour <askarpourdev@gmail.com>
2023-04-05 10:04:48 -0400

    refactor: remove swagger

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
 import createError from 'http-errors'
-import swaggerUI from 'swagger-ui-express'
 
 import connectDB from './config/database.js'
-import { swaggerSetup } from './config/swagger.js'
 import { isDevelopment, appListener, appErrorHandler, port } from './config/app.js'
 
 import { morganMiddleware } from './middlewares/morgan.js'
@@ -23,9 +21,6 @@ if (isDevelopment) app.use(morganMiddleware)
 app.use(cors({ origin: '*' }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
-// settings
-app.use('/api-doc', swaggerUI.serve, swaggerSetup)
 
 // routes
 app.use(allRoutes)
